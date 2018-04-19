ae479ca3a09e4e1d87e21bcef34aa6f529b368f1
tyfoster <foster.ty@gmail.com>
2018-04-18 22:35:55 -0600

    update app.js

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+  
+});.listen(process.env.PORT || 5000)
 
 module.exports = app;
